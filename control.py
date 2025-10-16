49e5ba78f83d2e69bdd0a4c24646f6c30b9e36aa
Nathaniel-Rabaya-CSUB <nrabaya@csub.edu>
2025-10-15 20:25:56 -0700

    Added print_info function + EUR-> USD conv

--- control.py
+++ control.py
@@ -4,27 +4,38 @@
 
 from pokemontcgsdk import *                         # import all data from pokemontcg library                          
 import requests                                     # import requests library
+from control import *
 
 base_url = "https://api.pokemontcg.io/v2/cards"     # base url for searching
 
-def get_card_info(card):                            # get card info from name function
-    url = f"{base_url}/{card}"                      # parce URL
+def get_card_info(poke_card):                            # get card info from name function
+    
+    url = f"{base_url}/{poke_card}"                      # parce URL
     response = requests.get(url)                    # GET from parced URL
 
     if response.status_code == 200:                 # If request was OK
-        card_data = response.json()                 # card_data = python dict from response
-        return card_data                            # return data
+        card_info = response.json()                 # card_data = python dict from response
+        card_info = card_info["data"]               # parse to object name "data" (why they make it like this ???)
+        print_info(card_info)
 
     else:                                           # Request failed w/ temp terminal response
         print("The card you were trying" \
         " to find does not exist\n")
 
-poke_card = input(str("Input Pokemon Card ID: "))   # temp terminal input (MUST BE ID  |  name search later)
-card_info = get_card_info(poke_card)                # execute function with input and output
-
-if card_info:                                       # if got valid response + info
-    card_info = card_info["data"]                   # parse to object name "data" (why they make it like this ???)
+def print_info(card_info):
+    # PRICE CONVERSION ===========================================================================#
+    con_price = (requests.get("https://open.er-api.com/v6/latest/EUR")).json() # Get all EUR conversions into json
     
-    print(f"{card_info["name"]})")                  # (temp) print some stuff
-    print(f"{card_info["supertype"]})")
-    print(f"{card_info["subtypes"]})")
+    eur_price = card_info["cardmarket"]["prices"]["averageSellPrice"]
+    usd_price = con_price["rates"]["USD"] * eur_price
+
+    usd_price = "{:.2f}".format(usd_price)
+    #=============================================================================================#
+
+    # * temp *
+    # PRINT INFO =================================================================================#
+    print(f"Name: {card_info["name"]}")
+    print(f"Supertype: {card_info["supertype"]}")
+    print(f"Rarity: {card_info["rarity"]}")
+    print(f"Price: USD ${usd_price}")
+    #=============================================================================================#
